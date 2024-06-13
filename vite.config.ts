0676df879e5af292c8eef1682a33d8c6dafc5f30
gastonfalena <gastonfalena@gmail.com>
2024-06-13 20:29:44 -0300

    add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,19 @@
 //Cual va a ser el entry point de nuestra aplicacion
+//yarn add -D @types/node
+//yarn add -D vite-plugin-dts declaration typescript files
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'lvlup-js-utils',
+    },
+    sourcemap: true,
+    target: 'es6',
+    minify: false,
+  },
+  plugins: [dts({ outDir: 'dist' })],
+})
